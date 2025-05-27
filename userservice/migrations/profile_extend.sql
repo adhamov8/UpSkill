d4191b2561f37156d3283ab19ae59e00afe09850
ALIBEK <151410023+adhamov8@users.noreply.github.com>
2025-05-28 01:08:08 +0300

    rename

--- userservice/migrations/profile_extend.sql
+++ userservice/migrations/profile_extend.sql
@@ -1,4 +1,3 @@
--- userservice/migrations/20250527_profile_extend.sql
 ALTER TABLE profiles
     ADD COLUMN IF NOT EXISTS first_name VARCHAR(60),
     ADD COLUMN IF NOT EXISTS last_name  VARCHAR(60),
