e324564a28df56fd4fd663e572c871924dc7b114
ALIBEK <151410023+adhamov8@users.noreply.github.com>
2025-05-28 01:08:58 +0300

    rename

---
+++ userservice/migrations/mentor_seed.sql
@@ -0,0 +1,22 @@
+INSERT INTO mentors (first_name,last_name,track,education,age,gender,skills,about,avatar_url,contact_url)
+VALUES
+-- Алексей Смирнов
+('Алексей','Смирнов','Data Science','МГУ, Прикладная математика',34,'M',
+ 'ML,Python',
+ 'Senior DS engineer',
+ 'https://i.pravatar.cc/150?u=aleksei',
+ 'mailto:alexey@example.com'),
+
+-- Екатерина Иванова
+('Екатерина','Иванова','Frontend','ИТМО, Информатика',29,'F',
+ 'React,UI/UX',
+ 'Frontend lead',
+ 'https://i.pravatar.cc/150?u=ekaterina',
+ 'tg://resolve?domain=katya_front'),
+
+-- Дмитрий Кузнецов
+('Дмитрий','Кузнецов','DevOps','СПбГУ, Физика',41,'M',
+ 'Kubernetes,AWS',
+ 'DevOps architect',
+ 'https://i.pravatar.cc/150?u=dmitry',
+ 'tg://resolve?domain=dmitry_devops');
